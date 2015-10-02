4aa4eb5bce32421d669c37f69d1449e56393b074
frash23 <jacob@bytesizedpacket.com>
2015-10-02 02:25:12 +0200

    Minor code re-structuring

--- server/server.js
+++ server/server.js
@@ -1,28 +1,31 @@
 "use strict";
 
 var authorizedSockets = [];
+var bcrypt = require('bcrypt'); // Use 13 rounds
 var io = require('socket.io')(undefined, {serveClient: false});
 io.on('connection', socketConnect);
 io.listen(7566);
 
 function socketConnect(sock) {
+	var authorized = false;
 	log('New connection: Socket ID '+ sock.id +' from IP '+ sock.handshake.address);
+
 	sock.emit('debug', 'Verified connection working both ways');
 
-	sock.on( 'disconnect', ()=> socketDisconnect(sock) );
-	sock.on( 'auth', data=> auth(sock, data.user, data.passhash) );
+	sock.on('auth', data=> {
+		log('Auth req: '+ data.user +', '+ data.passhash);
+	});
+	
+	sock.on('disconnect', ()=> {
+		var authIndex = authorizedSockets.indexOf( sock.id );
+		if(authIndex > -1) {
+			log('Authorized socket '+ sock.id +' disconnected.');
+			authorizedSockets[authIndex] = 0;
+		} else log('Unathorized socket '+ sock.id +' disconneceted');
+	});
 }
 
-function socketDisconnect(sock) {
-	var authIndex = authorizedSockets.indexOf( sock.id );
-	if(authIndex > -1) {
-		log('Authorized socket '+ sock.id +' disconnected.');
-		authorizedSockets[authIndex] = 0;
-	} else log('Unathorized socket '+ sock.id +' disconneceted');
-};
-
 function auth(sock, user, passhash) {
-	log('Auth req: '+ user +', '+ passhash);
 }
 
 function log() {
