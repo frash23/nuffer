3250dc82e480348ff5f4af8dfc03a3b7f42d7172
frash23 <jacob@bytesizedpacket.com>
2015-10-03 00:44:44 +0200

    Add bcrypt pre-hashing secure non-TLS connections

--- server/server.js
+++ server/server.js
@@ -3,35 +3,60 @@
 var authorizedSockets = [];
 var bcrypt = require('bcrypt'); // Use 13 rounds
 var io = require('socket.io')(undefined, {serveClient: false});
-io.on('connection', socketConnect);
+io.on( 'connection', socket=> socketConnect.call(socket) );
 io.listen(7566);
 
-function socketConnect(sock) {
+function socketConnect() {
 	var authorized = false;
-	log('New connection: Socket ID '+ sock.id +' from IP '+ sock.handshake.address);
+	var authorizing = false;
+	log('New connection: Socket ID '+ this.id +' from IP '+ this.handshake.address);
 
-	sock.emit('debug', 'Verified connection working both ways');
+	this.emit('debug', 'Verified connection working both ways');
 
-	sock.on('auth', data=> {
+	this.on('auth', data=> {
+		if(authorizing || authorized) return false;
+
 		log('Auth req: '+ data.user +', '+ data.passhash);
+		authorizing = true;
+		hashPass(data.passhash, hash=> {
+			authorizing = false;
+			if(hash === null) log('WARN: Problematic socket: '+ this.id);
+			else {
+				log(hash);
+				authorized = true;
+				bcrypt.compare('342853667818162828ca12542f0fdcb3dd701a9db6e45bcff275bf0436210525', hash, log);
+			}
+		});
 	});
 	
-	sock.on('disconnect', ()=> {
-		var authIndex = authorizedSockets.indexOf( sock.id );
+	this.on('disconnect', ()=> {
+		var authIndex = authorizedSockets.indexOf(this.id);
 		if(authIndex > -1) {
-			log('Authorized socket '+ sock.id +' disconnected.');
+			log('Authorized socket '+ this.id +' disconnected.');
 			authorizedSockets[authIndex] = 0;
-		} else log('Unathorized socket '+ sock.id +' disconneceted');
+		} else log('Unathorized socket '+ this.id +' disconneceted');
 	});
 }
 
-function auth(sock, user, passhash) {
+function hashPass(passhash, cb) {
+	var start = Date.now();
+	var hashTime = 1000;
+	bcrypt.hash( String(passhash), 13, (err, hash)=> {
+		if(err) { hash = null; log('WARN: Error while authorizing', err); }
+		/* If we spent less time than `hashTime`, wait. This avoids timing attacks. */
+		var timeToCb = Date.now() - start < hashTime;
+		timeToCb = timeToCb < 1? 0 : timeToCb;
+		setTimeout(cb, timeToCb, hash);
+	});
 }
 
 function log() {
 	var toLog = Array.prototype.slice.call(arguments);
 	var date = new Date();
-	var logPrefix = '['+ date.getHours() +':'+ date.getMinutes() +':'+ date.getSeconds() +']';
+	var logTime = ('0'+date.getHours()).substr(-2) +':';
+	logTime += ('0'+date.getMinutes()).substr(-2) +':';
+	logTime += ('0'+date.getSeconds()).substr(-2);
+	var logPrefix = '['+ logTime +']';
 	toLog.unshift(logPrefix);
 	console.log.apply(console, toLog);
 	//logToFile( toLog.join(' ') );
